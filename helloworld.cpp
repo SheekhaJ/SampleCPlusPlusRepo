ae774c383131134b78ac6ae39e5fd06f47f34688
Sheekha Jariwala <sheekhaj4893@gmail.com>
2018-12-13 03:48:13 -0800

    Added few notes regarding return types

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void sampleFunctionWithVoidReturnType(){
+    cout << "Inside sample function with void return type.";
+}
+
+int sampleFunctionWithIntReturnType(){
+    return 7;
+    return 9;
+}
+
 int main(){
     //Single-line comment
     std::cout << "Hello World!" <<endl;
@@ -14,6 +23,14 @@ int main(){
     std::cin >> x;
     std::cin.get(); 
     std::cout<< "Value entered for variable x is " << x <<std::endl;
+
+    sampleFunctionWithVoidReturnType();
+
+    //Following line has to be commented because function with return type as void can't be used with << operator. Compile-time error.
+    //std::cout << sampleFunctionWithVoidReturnType();
+
+    std::cin.get(); 
+    std::cout << sampleFunctionWithIntReturnType();
 
     std::cin.get(); 
 }
