daff01aae4f8f43fa70f6c760bc43d83a5ac91ca
Sheekha Jariwala <sheekhaj4893@gmail.com>
2018-12-13 04:30:46 -0800

    linked another file and added it to compilation command

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,10 +5,13 @@ void sampleFunctionWithVoidReturnType(){
     cout << "Inside sample function with void return type.";
 }
 
-int sampleFunctionWithIntReturnType(){
-    return 7;
+int sampleFunctionWithIntReturnType(int x){
+    std::cout << "Remember that C++ uses parameters using pass-by-value method.";
+    return x;
     return 9;
 }
+
+int add(int, int);
 
 int main(){
     //Single-line comment
@@ -16,7 +19,7 @@ int main(){
     std::cout << "Hello World in new line \n <----- This works in the same way";
     /* Block comment */
 
-    //std::cout with << indicating that data is moving from r-value to cout while printing
+    /* //std::cout with << indicating that data is moving from r-value to cout while printing
     std::cout << "\nEnter value for variable x: ";
     int x;
     //std::cin with >> indicating that data is moving from l-value cin to variable x while reading from the console. 
@@ -29,8 +32,10 @@ int main(){
     //Following line has to be commented because function with return type as void can't be used with << operator. Compile-time error.
     //std::cout << sampleFunctionWithVoidReturnType();
 
-    std::cin.get(); 
-    std::cout << sampleFunctionWithIntReturnType();
+    std::cin.get();  
+    std::cout << sampleFunctionWithIntReturnType(x);*/
+
+    std::cout << "Sum of two numbers calculated using add from another file is " << add(45, 60) <<endl;
 
     std::cin.get(); 
 }
