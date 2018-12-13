5a9b1d15ab574c922b060a45bc500a5786158c28
Sheekha Jariwala <sheekhaj4893@gmail.com>
2018-12-13 03:24:09 -0800

    Added code and notes to read from and write to console

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main(){
+    //Single-line comment
     std::cout << "Hello World!" <<endl;
-    cin.get(); 
+    std::cout << "Hello World in new line \n <----- This works in the same way";
+    /* Block comment */
+
+    //std::cout with << indicating that data is moving from r-value to cout while printing
+    std::cout << "\nEnter value for variable x: ";
+    int x;
+    //std::cin with >> indicating that data is moving from l-value cin to variable x while reading from the console. 
+    std::cin >> x;
+    std::cin.get(); 
+    std::cout<< "Value entered for variable x is " << x <<std::endl;
+
+    std::cin.get(); 
 }
